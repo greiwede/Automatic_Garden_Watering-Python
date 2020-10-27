7b0e009452817b2b6993ccf508c7eab6818ccf3f
Raukze <65811507+Raukze@users.noreply.github.com>
2020-10-27 12:23:15 +0100

    Settings fix

--- sprinkler/settings.py
+++ sprinkler/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webapp.apps.WebappConfig',
-    'django_extensions',
     'django_crontab',
     'django_celery_results',
     'django_celery_beat',
